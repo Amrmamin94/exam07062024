1fbab7613da4cbea08febad9a57a1e9610d9144c
Amrmamin94 <amrmamin94@gmail.com>
2024-06-08 00:40:37 +0300

    exam

---
+++ exam.py
@@ -0,0 +1,46 @@
+# option one
+
+# print('enter your choice')
+
+# x = 1
+
+# print('enter data employee') if x!=1 else print('invalid range try again')
+
+# print('Add new emplyee', input('enter number:'))     
+
+# print('print all emplyees', input('enter number:'))     
+
+# print('delate by age', input('enter number:'))     
+
+# print('update salary by name', input('enter number:'))     
+
+
+# option two
+
+# employee1 = ['mostafa saad','age', 20, 'salary', 555]
+# employee2 = ['asmaa','age', 25, 'salary', 1000]
+# employee3 = ['belal mostafa','age', 30,'salary', 3000]
+# employee4 = ['ziad','age', 32, 'salary', 320]
+
+# print(employee1,employee2,employee3,employee4)
+
+# option three
+
+# employees = ['employee1','employee2','employee3','employee4']
+
+# employees.remove('employee2')
+# employees.remove('employee3')
+
+# print(employees)
+
+
+# option four
+
+
+massege for eng. omar
+iam so sorry .. i tried my best ..i hadn't study lesson 8 and 9 .. i really have no time .. i m just study in vacation friday and saturdy
+so i had never make any task .. didnot solve proplems before .. this was the result which made me so sad 
+i promise i will solve this proplem to find a time to study .. i will solve all the tasks which is sent befor and i will send it back
+and i saved the video of the exam and i will solve it and send it to you
+i will use the time of the feast vacation to study and solve more .. i have no time except this time
+but kindly i need your support to send the time managment way that you told us about
